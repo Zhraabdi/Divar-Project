59b23c390ac004402bcca4c15109858251febcd4
zahra abdi <zahraw20000@gmail.com>
2025-11-12 22:18:50 +0330

    loginForm style

--- client/src/components/templates/CheckOtpForm.jsx
+++ client/src/components/templates/CheckOtpForm.jsx
@@ -21,16 +21,17 @@ function CheckOtpForm({code, setCode, mobile, setStep}) {
     }
 
   return (
-    <form onSubmit={submitHandler}>
-        <p>ورود به حساب کاربری</p>
-        <h3>کد تأیید را وارد کنید</h3>
-        <p>کد پیامک‌ شده به شمارۀ «{mobile}» را وارد کنید.</p>
-        <label htmlFor="input">کد تایید را وارد کندی</label>
-        <input type="text" id="input" placeholder="'کد تایید" value={code} onChange={(e) => setCode(e.target.value)}/>
+    <div className="min-h-screen flex items-center justify-center">
+    <form onSubmit={submitHandler} className="max-w-[600px] w-full flex flex-col border border-solid border-gray-300 rounded-md p-8">
+        <p className="text-lg font-normal mb-5">ورود به حساب کاربری</p>
+        <h3 className="text-lg font-normal mb-10">کد تأیید را وارد کنید</h3>
+        <span className="text-gray-600 text-sm mb-5">کد پیامک‌ شده به شمارۀ «{mobile}» را وارد کنید.</span>
+        <input className="mt-[10px] mx-0 mb-5 border border-solid rounded-[5px] p-2 focus:outline-none focus:border-[#a62626] focus:bg-white" type="text" id="input" placeholder="'کد تایید" value={code} onChange={(e) => setCode(e.target.value)}/>
 
-        <button type="submit">ورود</button>
-        <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+        <button className="w-[120px] px-[10px] py-[5px] border-none bg-[#a62626] text-white rounded-[5px] cursor-pointer" type="submit">ورود</button>
+        <button className="w-[170px] bg-white text-[#a62626] border-[#a62626] border border-solid mt-8 rounded-[5px] p-1" onClick={() => setStep(1)}>تغییر شماره موبایل</button>
     </form>
+    </div>
   )
 }
 
